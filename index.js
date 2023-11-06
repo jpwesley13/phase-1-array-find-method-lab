7067c85aa1d5dc07706f15fca929da9beebf4e15
Jared Patterson <jpwesley34@gmail.com>
2023-11-06 13:22:07 -0800

    speedruns

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+/*function superbowlWin(history){
+    for (const season of history){
+        if (season.result === "W"){
+            return season.year
+        }
+    }
+}*/
+//My way above works, but it doesn't use the find() method
+
+function superbowlWin(history){
+    const dub = history.find(({result}) => result === 'W');
+    if (dub){
+    return win.year;
+    }
+}
